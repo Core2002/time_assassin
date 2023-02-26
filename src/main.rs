3c7cfa164ce1a099b40f5cf5b1f66146f4b8dc45
NekokeCore <Core2002@163.com>
2023-02-26 23:53:48 +0800

    画好了时间看板

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,35 @@
-use crate::data_util::up_date_data;
+use crate::data_util::update_data;
 
 mod data_util;
 
+struct YearPanel {
+    // year | count of start empty block | count of blocks in a year
+    day_flag: [usize; 3],
+    // 7 x 53 blocks for panel
+    day_arr: [[usize; 7]; 53],
+}
+
 fn main() {
-    up_date_data();
-    println!("Hello, world!");
+    let y2018 = YearPanel {
+        day_flag: [2018, 1, 365],
+        day_arr: [[0; 7]; 53],
+    };
+
+    print_year_panel(y2018);
+}
+
+fn print_year_panel(yp: YearPanel) {
+    for iw in 0..yp.day_arr.len() {
+        for id in 0..yp.day_arr[iw].len() {
+            if iw * 7 + id > yp.day_flag[2] {
+                break;
+            }
+            if iw <= yp.day_flag[1] / 7 && id < yp.day_flag[2] % 7 {
+                print!("X")
+            } else {
+                print!("{}", id)
+            }
+        }
+        println!(" - {}", iw + 1)
+    }
 }
