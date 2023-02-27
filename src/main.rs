8a659db09857d293c03cbec2049e0ee8113299a3
NekokeCore <Core2002@163.com>
2023-02-27 12:58:03 +0800

    修正算法

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,7 @@ struct YearPanel {
 }
 
 fn main() {
+    println!("2018");
     let mut y2018 = YearPanel {
         day_flag: [2018, 1, 365],
         day_arr: [[0; 7]; 53],
@@ -17,11 +18,20 @@ fn main() {
 
     init_year_panel(&mut y2018);
     print_year_panel_l(y2018);
+
+    println!("2019");
+    let mut y2019 = YearPanel {
+        day_flag: [2019, 2, 365],
+        day_arr: [[0; 7]; 53],
+    };
+
+    init_year_panel(&mut y2019);
+    print_year_panel_l(y2019);
 }
 
 // 横着输出
 fn print_year_panel_l(yp: YearPanel) {
-    for id in 0..7 {
+    for id in 0..yp.day_arr[0].len() {
         for iw in 0..yp.day_arr.len() {
             print!("{}", yp.day_arr[iw][id])
         }
@@ -33,14 +43,14 @@ fn print_year_panel_l(yp: YearPanel) {
 fn init_year_panel(yp: &mut YearPanel) {
     for iw in 0..yp.day_arr.len() {
         for id in 0..yp.day_arr[iw].len() {
-            if iw * 7 + id > yp.day_flag[2] {
-                yp.day_arr[iw][id] = 1;
+            if (iw + 1) * 7 + (id + 1) - yp.day_flag[1] > yp.day_flag[2] {
+                yp.day_arr[iw][id] = 2;
                 continue;
             }
-            if iw <= yp.day_flag[1] / 7 && id < yp.day_flag[2] % 7 {
+            if iw <= yp.day_flag[1] / 7 && id + 1 <= yp.day_flag[1] % 7 {
                 yp.day_arr[iw][id] = 1;
             } else {
-                yp.day_arr[iw][id] = 0;
+                yp.day_arr[iw][id] = 3;
             }
         }
     }
