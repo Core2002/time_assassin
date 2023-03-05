853ea1feec38fd15be5293d29d4009f6fe5f635a
NekokeCore <Core2002@163.com>
2023-03-05 13:21:13 +0800

    开启web版本

---
+++ app.js
@@ -0,0 +1,69 @@
+var YearPanel = /** @class */ (function () {
+    function YearPanel(day_flag) {
+        this.day_flag = day_flag;
+        var lw = 7;
+        var ld = 53;
+        this.day_arr = new Array(ld);
+        for (var i = 0; i < ld; i++) {
+            this.day_arr[i] = new Array(7);
+        }
+        for (var iw = 0; iw < ld; iw++) {
+            for (var id = 0; id < lw; id++) {
+                if ((iw + 1) * lw + (id + 1) - this.day_flag[1] > this.day_flag[2]) {
+                    this.day_arr[iw][id] = 1;
+                    continue;
+                }
+                if (iw <= this.day_flag[1] / lw && id + 1 <= this.day_flag[1] % lw) {
+                    this.day_arr[iw][id] = 0;
+                }
+                else {
+                    this.day_arr[iw][id] = 2;
+                }
+            }
+        }
+    }
+    YearPanel.prototype.print_year_panel_l = function () {
+        var res = "";
+        res += "--- " + this.day_flag[0] + " ---\n";
+        for (var id = 0; id < this.day_arr[0].length; id++) {
+            for (var iw = 0; iw < this.day_arr.length; iw++) {
+                res += this.day_arr[iw][id];
+                if (this.day_arr[iw][id] == 0 || this.day_arr[iw][id] == 1) {
+                    addWhiteBlock();
+                }
+                else if (this.day_arr[iw][id] == 2) {
+                    addBlock();
+                }
+            }
+            res += "\n";
+            addBr();
+        }
+        console.log(res);
+    };
+    return YearPanel;
+}());
+var y2018 = new YearPanel([2018, 1, 365]);
+y2018.print_year_panel_l();
+function addBr() {
+    addEle("<br>");
+}
+function addBlock() {
+    addEle("<button onclick=\"xwx(this)\" type=\"button\" class=\"layui-btn layui-btn-xs\" style=\"background-color: darkgrey;padding: 15;margin: 2px;\">...</button>");
+}
+function addWhiteBlock() {
+    addEle("<button type=\"button\" class=\"layui-btn layui-btn-xs\" style=\"background-color: white;padding: 15;margin: 2px;\">...</button>");
+}
+function addEle(html) {
+    var box = document.getElementById('qwq');
+    box.innerHTML = box.innerHTML + html;
+}
+function xwx(el) {
+    if (el.getAttribute("d") == "3") {
+        el.setAttribute("d", "2");
+        el.setAttribute("style", "background-color: darkgrey;margin: 2px;");
+    }
+    else {
+        el.setAttribute("d", "3");
+        el.setAttribute("style", "background-color: green;margin: 2px;");
+    }
+}
